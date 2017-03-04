8ecc3127b67e4e73e3d2686e22a5020e311c1334
earnest vekariya <epvekariya28@gmail.com>
2017-03-05 00:21:31 +0530

    Update keyboard.py

--- keyboard.py
+++ keyboard.py
@@ -3,28 +3,28 @@
 import RPi.GPIO as GPIO
 import time
 
-def keyboard_value():
-    GPIO.setmode(GPIO.BCM)
+def keyboard_value():           #function for reading keyboard input
+    GPIO.setmode(GPIO.BCM)      #initialize board as bcm
     GPIO.setwarnings(False)
-    GPIO.setup(2, GPIO.IN,pull_up_down=GPIO.PUD_UP)
-    GPIO.setup(3,GPIO.IN,pull_up_down=GPIO.PUD_UP)
-    GPIO.setup(4,GPIO.IN,pull_up_down=GPIO.PUD_UP)
-    GPIO.setup(17,GPIO.IN,pull_up_down=GPIO.PUD_UP)
-    GPIO.setup(20,GPIO.OUT)
-    GPIO.setup(21,GPIO.OUT)
-    GPIO.setup(18,GPIO.OUT)
-    GPIO.setup(23,GPIO.OUT)
-    value=[['1','2','3','A'],
+    GPIO.setup(2, GPIO.IN,pull_up_down=GPIO.PUD_UP)   #initialization of pin as input
+    GPIO.setup(3,GPIO.IN,pull_up_down=GPIO.PUD_UP)  #initialization of pin  as input
+    GPIO.setup(4,GPIO.IN,pull_up_down=GPIO.PUD_UP)  #initialization of pin  as input
+    GPIO.setup(17,GPIO.IN,pull_up_down=GPIO.PUD_UP)  #initialization of pin as input
+    GPIO.setup(20,GPIO.OUT) #initialization of pin as output
+    GPIO.setup(21,GPIO.OUT) #initialization of pin as output
+    GPIO.setup(18,GPIO.OUT) #initialization of pin as output
+    GPIO.setup(23,GPIO.OUT) #initialization of pin as output
+    value=[['1','2','3','A'],  #keypad array for output
            ['4','5','6','B'],
            ['7','8','9','C'],
            ['*','0','#','D']]
-    row=[21,20,18,23]
-    col=[2,3,4,17]
+    row=[21,20,18,23]  #controller pin connected to row of keypad
+    col=[2,3,4,17]     #controller pin connected to column of keypad 
     
-    for n in range(4):
+    for n in range(4):  #initialize all the output pin with high logic
         GPIO.output(row[n],True)
 
-    while(True):
+    while(True):            #loop will execute untill it finds which row and column are selected by press key
         
         for m in range(4):
 ##            time.sleep(0.05)
@@ -34,7 +34,7 @@ def keyboard_value():
                     ##print value[m][i]
                     while GPIO.input(col[i]) == 0:
                         pass
-                    return value[m][i]
+                    return value[m][i]      #return the press key value
             GPIO.output(row[m],True)
 <<<<<<< HEAD
 #while 1:
